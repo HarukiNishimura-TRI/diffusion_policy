04cf0dacbb7293d4fbfa31c424e9f3418b5760af
Haruki Nishimura <haruki.nishimura@tri.global>
2025-05-10 12:15:06 -0700

    filter some states to induce OOD

--- demo_playground.py
+++ demo_playground.py
@@ -14,21 +14,26 @@ from diffusion_policy.common.replay_buffer import ReplayBuffer
 from playground.envs import make_point_maze_env
 
 
+def filter_this_state(state: np.ndarray):
+    # TODO: make this function configurable.
+    if not len(state) >= 2:
+        raise ValueError("In-valid state specification.")
+    x = state[0]
+    y = state[1]
+    # filter = (0.0 < x < 2.0) & (-2.0 < y < 0.0)
+    filter = (0.0 < x < 2.0) & (0.0 < y < 2.0)
+    return filter
+
+
 def main(
     env_config_path: str,
     policy_model_dir: str,
     output_dir: str,
     num_episodes: str,
     action_includes_progress: bool = False,
+    filter_data: bool = False,
+    init_seed: int = 0,
 ):
-    """
-    Collect demonstration for the Playground task.
-
-    Usage: python demo_playground.py -o data/pusht_demo.zarr
-
-    This script is compatible with both Linux and MacOS.
-    """
-
     # Load config
     config = OmegaConf.load(env_config_path)
     eval_config = OmegaConf.merge(config, config.evaluation)  # eval mode.
@@ -54,14 +59,21 @@ def main(
         raise (NotImplementedError(config.rl.training.algorithm))
 
     # episode-level while loop
+    seed = init_seed
     while replay_buffer.n_episodes < num_episodes:
         episode = list()
         # record in seed order, starting with 0
-        seed = replay_buffer.n_episodes
         print(f"starting seed {seed}")
 
         # reset env and get observations (including info and render for recording)
         obs = eval_env.reset(seed=seed)
+        if filter_this_state(obs["achieved_goal"]):
+            print(
+                "Skipping this seed where initial (x, y) == "
+                f"({obs['achieved_goal'][0]}, {obs['achieved_goal'][1]})"
+            )
+            seed += 1
+            continue
         init_img = deepcopy(obs["rgb_stack"])
         img = deepcopy(init_img)
         if not config.env.return_rgb_observation:
@@ -81,6 +93,7 @@ def main(
             )
             data = {
                 "img": np.concatenate([img, init_img], axis=-1),
+                "achieved_goal": np.float32(obs["achieved_goal"]),
                 "state": np.float32(state),
                 "action": np.float32(act),
             }
@@ -103,12 +116,27 @@ def main(
                 data = episode[idx]
                 data["action"] = np.concatenate([data["action"], [progress]])
 
+        if filter_data:
+            # Filter out data
+            filtered_episode = list()
+            for data in episode:
+                if not filter_this_state(data["achieved_goal"]):
+                    filtered_episode.append(data)
+            if len(filtered_episode) < len(episode):
+                filtered_steps = len(episode) - len(filtered_episode)
+                print(
+                    f"Filtered {filtered_steps} steps "
+                    f"({round(filtered_steps / len(episode) * 100, 1)}% reduction)."
+                )
+            episode = filtered_episode
+
         # save episode buffer to replay buffer (on disk)
         data_dict = dict()
         for key in episode[0].keys():
             data_dict[key] = np.stack([x[key] for x in episode])
         replay_buffer.add_episode(data_dict, compressors="disk")
         print(f"saved seed {seed}")
+        seed += 1
 
 
 if __name__ == "__main__":
@@ -147,6 +175,18 @@ if __name__ == "__main__":
             "If True, action includes normalized task progress. Defaults to False."
         ),
     )
+    parser.add_argument(
+        "--filter_data",
+        action="store_true",
+        default=False,
+        help=("If True, data is filtered so certain states become OOD."),
+    )
+    parser.add_argument(
+        "--init_seed",
+        type=int,
+        default=0,
+        help=("Initial seed value for demonstration. Defaults to 0."),
+    )
     args = parser.parse_args()
     policy_model_name = args.rl_model_name
     action_includes_progress = args.action_includes_progress
@@ -164,10 +204,14 @@ if __name__ == "__main__":
         current_dir, f"../playground/logs/{policy_model_name}/models"
     )
     output_dir = os.path.join(current_dir, f"../data/playground/{zarr_name}")
+    filter_data = args.filter_data
+    init_seed = args.init_seed
     main(
         env_config_path,
         policy_model_dir,
         output_dir,
         num_episodes,
         action_includes_progress,
+        filter_data,
+        init_seed,
     )
